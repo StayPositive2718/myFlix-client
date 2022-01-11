f89b0c32ae46d384099c4bab8fbeaaf7d9908260
Matt Howell <elwhwl@gmail.com>
2022-01-11 10:55:53 -0500

    renamed main-view, export main-view.jsx

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class MainView extends React.Component {
+
+  render() {
+    return (
+      <div className="main-view">
+        <div>Indiana Jones and the Raiders of the Lost Ark</div>
+        <div>The Big Lebowski</div>
+        <div>The Goonies</div>
+      </div>
+    );
+  }
+}
+
+export default MainView;
