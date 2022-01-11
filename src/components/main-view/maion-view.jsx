fce40e7720e2b72fbb43863ad9f10db8241f679a
Matt Howell <elwhwl@gmail.com>
2022-01-10 22:21:27 -0500

    add components folder, main-view folder, and main-view.jsx.

---
+++ src/components/main-view/maion-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+
+  render() {
+    return (
+      <div className="main-view">
+        <div>Indiana Jones and the Raiders of the Lost Ark</div>
+        <div>The Big Lebowski</div>
+        <div>The Goonies</div>
+      </div>
+    );
+  }
+}
