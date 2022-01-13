4ecf74affdf6683a0fdd78839459ffb5e43cfb26
Matt Howell <elwhwl@gmail.com>
2022-01-13 12:24:15 -0500

    add movie-card and movie-view componments

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      movies: [
+        { _id: 1, Title: 'Indianas Jones and the Raiders of the Lost Ark', Description: 'description 1...', ImagePath: '...' },
+        { _id: 2, Title: 'The Big Lebowski', Description: 'description 2...', ImagePath: '...' },
+        { _id: 3, Title: 'The Goonies', Description: 'description 3...', ImagePath: '...' }
+      ]
+    }
+  }
 
   render() {
+    const movies = this.state.movies;
+
+    if (movies.length === 0) {
+      return <div ClassName="main-view">The list is empty!</div>;
+    }
+
     return (
       <div className="main-view">
-        <div>Indiana Jones and the Raiders of the Lost Ark</div>
-        <div>The Big Lebowski</div>
-        <div>The Goonies</div>
+        {movies.map(movie => <MovieCard />)}
       </div>
     );
   }
 }
-
-export default MainView;
