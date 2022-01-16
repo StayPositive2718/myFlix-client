3970f48d91f4fc21b6276666158c7d24cbb7307b
Matt Howell <elwhwl@gmail.com>
2022-01-16 15:15:34 -0500

    work on url problem

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,7 +8,7 @@ export class MovieView extends React.Component {
     return (
       <div className="movie-view">
         <div className="movie-poster">
-          <img src={movie.ImagePath} />
+          <img src={movie.ImagePath + "?not-from-cache-please"} crossOrigin="Anonymous" />
         </div>
         <div className="movie-title">
           <span className="label">Title: </span>
